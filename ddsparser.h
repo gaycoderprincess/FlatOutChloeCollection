ee3b17c3599f3e7c434424bebdee23ac7f4be8a4
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-07-30 00:28:47 +0200

    refactor dds parser

--- ddsparser.h
+++ ddsparser.h
@@ -1,27 +1,13 @@
-bool IsNativelySupportedDDSFormat(uint32_t format) {
-	if (format == 0x31545844) return true; // DXT1
-	if (format == 0x33545844) return true; // DXT3
-	if (format == 0x34545844) return true; // DXT4
-	if (format == 0x35545844) return true; // DXT5
-	return false;
-}
-
-DevTexture* __thiscall CreateTextureFromMemoryNew(DeviceD3d* pThis, DevTexture* pTexture, uint8_t* data, uint32_t dataSize, uint32_t flags) {
-	if (IsNativelySupportedDDSFormat(*(uint32_t*)(&data[0x54]))) {
-		return DeviceD3d::CreateTextureFromMemory(pThis, pTexture, data, dataSize, flags);
-	}
-
+bool CreateCustomTexture(DevTexture*& pTexture, uint8_t* data, uint32_t dataSize, uint32_t flags) {
 	if (!pTexture) {
 		// small dummy texture for constructing a DevTexture
-		pTexture = DeviceD3d::CreateTextureFromFile(pThis, pTexture, "data/global/overlay/overlaynitro.dds", flags);
+		pTexture = DeviceD3d::CreateTextureFromFile(pDeviceD3d, pTexture, "data/global/overlay/overlaynitro.dds", flags);
 	}
 
 	if (pTexture->pD3DTexture) {
 		pTexture->pD3DTexture->Release();
 		pTexture->pD3DTexture = nullptr;
 	}
-
-	WriteLog(std::format("loading texture {} with size {}", pTexture->sPath.Get(), dataSize));
 
 	// fix header
 	if (data[0x4C] == 0x18) {
@@ -31,11 +17,18 @@ DevTexture* __thiscall CreateTextureFromMemoryNew(DeviceD3d* pThis, DevTexture* 
 	auto hr = D3DXCreateTextureFromFileInMemory(pDeviceD3d->pD3DDevice, data, dataSize, &pTexture->pD3DTexture);
 	if (hr != S_OK) {
 		WriteLog(std::format("Failed to load {} - {:X}", pTexture->sPath.Get(), (uint32_t)hr));
-		return nullptr;
+		return false;
 	}
 
 	pTexture->nLoadState = 5;
-	return pTexture;
+	return true;
+}
+
+DevTexture* __thiscall CreateTextureFromMemoryNew(DeviceD3d* pThis, DevTexture* pTexture, uint8_t* data, uint32_t dataSize, uint32_t flags) {
+	if (CreateCustomTexture(pTexture, data, dataSize, flags)) {
+		return pTexture;
+	}
+	return nullptr;
 }
 
 DevTexture* __thiscall CreateTextureFromFileNew(DeviceD3d* pThis, DevTexture* pTexture, const char* _path, uint32_t flags) {
@@ -56,32 +49,13 @@ DevTexture* __thiscall CreateTextureFromFileNew(DeviceD3d* pThis, DevTexture* pT
 	file.Load(path.c_str(), 9);
 
 	uint32_t dataSize = file.pFileCodec->GetFileSize();
-	WriteLog(std::format("loading texture {} with size {}", path, dataSize));
 
 	auto data = new uint8_t[dataSize];
 	file.pFileCodec->ReadBytes(data, dataSize);
-
-	if (pTexture->pD3DTexture) {
-		pTexture->pD3DTexture->Release();
-		pTexture->pD3DTexture = nullptr;
+	if (CreateCustomTexture(pTexture, data, dataSize, flags)) {
+		return pTexture;
 	}
-
-	WriteLog(std::format("loading texture {} with size {}", pTexture->sPath.Get(), dataSize));
-
-	// fix header
-	if (data[0x4C] == 0x18) {
-		data[0x4C] = 0x20;
-	}
-
-	auto hr = D3DXCreateTextureFromFileInMemory(pDeviceD3d->pD3DDevice, data, dataSize, &pTexture->pD3DTexture);
-	delete[] data;
-	if (hr != S_OK) {
-		WriteLog(std::format("Failed to load {} - {:X}", pTexture->sPath.Get(), (uint32_t)hr));
-		return nullptr;
-	}
-
-	pTexture->nLoadState = 5;
-	return pTexture;
+	return nullptr;
 }
 
 void ApplyDDSParserPatches() {
