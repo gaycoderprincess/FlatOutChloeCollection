1040e2894b4c94e1ceaa9283979d04164b27ce03
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-10-02 20:27:05 +0200

    Update config.h

--- config.h
+++ config.h
@@ -61,7 +61,7 @@ uint32_t nFO2Weathers = 1;
 uint32_t nMPInterpolation = 1;
 uint32_t nDefaultTickRate = 1;
 uint32_t nAllowCheats = 0;
-uint32_t nJoinNotifs = 0;
+uint32_t nJoinNotifs = 1;
 
 uint32_t nMultiplayerHandlingMode = HANDLING_NORMAL;
 
